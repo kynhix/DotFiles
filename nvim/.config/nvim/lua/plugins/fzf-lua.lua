8e6592a3c31d3cca602b2c1671fe9148a5b01cf0
kynhix <kristopher.t.hicks@gmail.com>
2026-02-05 14:42:10 -0500

    Update

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -7,20 +7,6 @@ return {
       grep = {
         rg_opts = "--column --line-number --no-heading --color=always --smart-case --max-columns=4096 --sort path -e",
         actions = {
-          ["ctrl-y"] = {
-            fn = function(selected, opts)
-              local path = require("fzf-lua.path")
-              local entry = path.entry_to_file(selected[1], opts)
-
-              if entry and entry.path then
-                vim.fn.setreg('"', entry.path)
-                vim.fn.setreg("+", entry.path)
-                vim.notify("Copied file path: " .. entry.path)
-              end
-            end,
-            desc = "copy file path",
-            exec_silent = true,
-          },
           ["ctrl-o"] = { fn = require("fzf-lua").actions.file_edit, reload = true },
         },
       },
