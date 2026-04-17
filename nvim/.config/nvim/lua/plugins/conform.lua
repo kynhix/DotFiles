ac351cfd08704b394b76ae20b16b53eade30a925
kynhix <kristopher.t.hicks@gmail.com>
2026-04-16 22:25:54 -0400

    Only format with biome in chess project

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,13 +2,17 @@ return {
   "stevearc/conform.nvim",
   optional = true,
   opts = function(_, opts)
-    -- Make sure the formatters_by_ft table exists
     opts.formatters_by_ft = opts.formatters_by_ft or {}
-
-    -- Set formatters for Vue files to use only biome
-    opts.formatters_by_ft.vue = { "biome" }
-
-    -- If you want to ensure biome formatter is configured properly
     opts.formatters = opts.formatters or {}
+
+    -- Use biome for Vue files only in chess projects
+    opts.formatters_by_ft.vue = function(bufnr)
+      local bufname = vim.api.nvim_buf_get_name(bufnr)
+      if bufname:match("/chess/") then
+        return { "biome" }
+      end
+      return {}
+    end
+
   end,
 }
